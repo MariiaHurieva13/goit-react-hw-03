ac7b3496295c4ab8a166398b68527b70e949ea55
MariiaHurieva13 <Marygureva@gmail.com>
2025-04-15 17:44:23 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,52 @@
+import { useEffect, useState } from "react";
 
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import "./App.css";
+import saveToLs from "../../utils/saveToLs";
+import getFromLs from "../../utils/getFromLs";
+
+import defaultContacts from "../../assets/data/defaultContacts.json";
+
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = getFromLs("contacts");
+
+    return savedContacts && savedContacts.length !== 0
+      ? savedContacts
+      : defaultContacts;
+  });
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    saveToLs("contacts", contacts);
+  }, [contacts]);
+
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name
+      .toLocaleLowerCase()
+      .includes(filter.trim().toLocaleLowerCase());
+  });
+
+  const addNewContact = (newContact) => {
+    setContacts((prewContacts) => [...prewContacts, newContact]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts((prewContacts) =>
+      prewContacts.filter((contact) => contact.id !== id)
+    );
+  };
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addNewContact} />
+      <SearchBox onFilter={setFilter} filter={filter} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+    </>
+  );
+}
+
+export default App;
